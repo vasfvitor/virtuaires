b85eea8047a0bf8b9c6d6e7729ac4012e91a6a87
vasfvitor <gitkey@virtuaires.com.br>
2023-11-04 00:21:12 -0300

    dynamic images

---
+++ src/components/images.ts
@@ -0,0 +1,47 @@
+import type { ImageMetadata } from 'astro';
+
+const load = async function () {
+    let images: Record<string, () => Promise<unknown>> | undefined = undefined;
+    try {
+        images = import.meta.glob('/images/**/*.{jpeg,jpg,png,tiff,webp,gif,svg,JPEG,JPG,PNG,TIFF,WEBP,GIF,SVG}');
+    } catch (e) {
+        // continue regardless of error
+    }
+    return images;
+};
+
+let _images: Record<string, () => Promise<unknown>> | undefined = undefined;
+
+/** */
+export const fetchLocalImages = async () => {
+    _images = _images || (await load());
+    return _images;
+};
+
+/** */
+export const findImage = async (
+    imagePath?: string | ImageMetadata | null
+): Promise<string | ImageMetadata | undefined | null> => {
+    // Not string
+    if (typeof imagePath !== 'string') {
+        return imagePath;
+    }
+
+    // Absolute paths
+    if (imagePath.startsWith('http://') || imagePath.startsWith('https://') || imagePath.startsWith('/')) {
+        return imagePath;
+    }
+
+    // Relative paths or not "~/assets/"
+    if (!imagePath.startsWith('~/assets/images')) {
+        return imagePath;
+    }
+
+    const images = await fetchLocalImages();
+    const key = imagePath.replace('~/', '/src/');
+
+    return images && typeof images[key] === 'function'
+        ? ((await images[key]()) as { default: ImageMetadata })['default']
+        : null;
+};
+
