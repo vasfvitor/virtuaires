f8b94ea1a8fe96d13d25ddf0fbd134ee6654597f
vasfvitor <gitkey@virtuaires.com.br>
2023-11-15 18:42:57 -0300

    redesign

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = ":D";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Vitor Ayres";
+export const SITE_DESCRIPTION = "Who am I? Where am I?";
